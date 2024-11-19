01be69e9c35ff66db287ecac45ad1d0efe36c81b
DoriaFabio <doriafabio2001@gmail.com>
2024-11-19 22:40:20 +0100

    do card with js

--- script.js
+++ script.js
@@ -17,8 +17,42 @@ console.clear();
 const baseUrl = "https://jsonplaceholder.typicode.com";
 const resource = "/photos";
 const endpoint = baseUrl + resource;
-const params = {"_limit": 6};
+const params = { "_limit": 6 };
+const card = document.getElementById("card");
 
-axios.get(baseUrl+resource,{params}).then((res) => {
-    console.log(res.data);
-});
+function getData() {
+    const arrayName = [];
+    let template = ``;
+    for (let i = 0; i < 6; i++) {
+        axios.get(baseUrl + resource, { params }).then((res) => {
+            console.log(res.data);
+            const image = res.data[i].url;
+            console.log(image);
+            const text = res.data[i].title;
+            console.log(text);
+            template += `
+            <div class="col d-flex flex-column">
+                <img id="pin" src="./img/pin.svg" alt="Pallino">
+                <div class="img">
+                    <img src="${image}" alt="Foto">
+                </div>
+                <div class="text">
+                    <p>${text}</p>
+                </div>
+                </div>
+            `;
+            arrayName.push(res.data);
+            if (arrayName.length === 6) {
+                card.innerHTML = template;
+            };
+        })
+            .catch((error) => {
+                console.log(error);
+            })
+
+            .finally(() => {
+                console.log("in tutti i casi eseguita");
+            });
+    };
+}
+getData();
